14ac79f9dffcd7b712232b205cac799fe6389bfa
Arda Sak <ardasak434@gmail.com>
2021-12-07 01:03:50 +0300

    Initial commit. Started learning dart :).

---
+++ bolum-1/hello_world.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("sa");
+  print("hello world");
+  print(3 + 5);
+  print('arda\'nın arabası');
+  print("arda'nın arabası");
+  print("arda" + " sak");
+
+  //burada ilk defa dart kodu yazıyorum
+}
